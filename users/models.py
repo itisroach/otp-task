ea0294d8c34b0428a8dc9495e22b568350641355
unknown <rouchashoori@gmail.com>
2025-07-19 17:20:46 +0330

    create otp model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin 
-from datetime import timezone
+from datetime import timezone, timedelta 
+import random
 
 
 
@@ -34,3 +35,17 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.mobile
+    
+
+
+class OTP(models.Model):
+    mobile = models.CharField(max_length=15)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    attempts = models.PositiveIntegerField(default=0)
+
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=5)
+
+    def __str__(self):
+        return f"OTP for {self.mobile}: {self.code}"
