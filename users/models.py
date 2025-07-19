014c4a03bc5e77cdbb0a96aa54dcc094d2e3def3
unknown <rouchashoori@gmail.com>
2025-07-19 16:48:40 +0330

    create custom user model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin 
+from datetime import timezone
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, mobile, password=None, **extra_fields):
+        if not mobile:
+            raise ValueError('Mobile number is required')
+        user = self.model(mobile=mobile, **extra_fields)
+        user.set_unusable_password()
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, mobile, password, **extra_fields):
+        user = self.create_user(mobile, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    mobile = models.CharField(max_length=15, unique=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'mobile'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.mobile
